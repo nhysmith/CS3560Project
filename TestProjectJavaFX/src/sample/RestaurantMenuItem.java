87ee33265130d9af47950900c05900e1df6ff74a
DavidMSegal <75288346+DavidMSegal@users.noreply.github.com>
2020-12-01 14:47:25 -0800

    Update RestaurantMenuItem.java

--- TestProjectJavaFX/src/sample/RestaurantMenuItem.java
+++ TestProjectJavaFX/src/sample/RestaurantMenuItem.java
@@ -6,11 +6,21 @@ public class RestaurantMenuItem
     String description;
     double price;
     String restaurantID;
+    int itemID;
 
     RestaurantMenuItem(String itemName, String description, double price)
     {
         this.itemName = itemName;
         this.description = description;
         this.price = price;
+    }
+    
+    RestaurantMenuItem(String itemName, String description, double price, String restaurantID, int itemID)
+    {
+        this.itemName = itemName;
+        this.description = description;
+        this.price = price;
+        this.restaurantID = restaurantID;
+        this.itemID = itemID
     }
 }
